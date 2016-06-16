87992380ca527eae14eaf7041e3581f47d6ff63c
César Trigo <cesartrigo@gmail.com>
2016-06-17 00:59:55 +0200

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
-#
-# (Place the script on after integration trigger)
-#
 # Author: Pablo Serrano
-#
-# You need add jenkins module "managed scripts" and set this Argument list:
+# Place the script after integration trigger)
+# You need to add Jenkins Managed Scripts Plugin and set this Argument list:
 # - appid
 # - name
 # - notes
 # - tags 
 #
-## Applivery Configuration
+# --- Applivery Configuration ---
 API_TOKEN="Put your applivery token"
+APP_NAME="app-debug"
+# -------------------------------
+# --- DO NOT EDIT BELOW HERE! ---
+# -------------------------------
 APP_ID="$1"
 NAME="$2"
 NOTES="$3"
 TAGS="$4"
-# BOT_NAME. Should be the bot name. By default is "{PRODUCT_NAME} bot".
-APP_NAME="app-debug"
-# DO NOT EDIT BELOW HERE!
-########################################
 # Last Integration Path
 INTEGRATION_PATH="app/build/outputs/apk"
 APK="${INTEGRATION_PATH}/${APP_NAME}.apk"
-echo "APK Path: ${APK}"
-echo "*** Uploading to Applivery ***"
+echo "# APK Path: ${APK}"
+echo "# Uploading to Applivery..."
 curl "https://dashboard.applivery.com/api/builds" -H "Authorization:${API_TOKEN}" -F app="${APP_ID}" -F versionName="${NAME}" -F notes="${NOTES}" -F os="android" -F tags="${TAGS}" -F package=@"${APK}"
-echo "Applivery upload finished!"
+echo "# Applivery upload success!"
